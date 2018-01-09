b178a69f01383a96a267cbd68d90944c48de4c2d
Andy2510 <andrius.venckunas@gmail.com>
2018-01-09 20:23:55 +0200

    second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sveiki gyvi
                 </div>
 
                 <div class="links">
